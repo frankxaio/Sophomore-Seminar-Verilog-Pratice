3857e483c935e0bdc0367b004b5086d8a340abaf
Xiao <frankxaio@gmail.com>
2023-04-21 00:51:34 +0800

    Homework 2-2

--- 大二專題/Lab/Homework 2-2/Top.v
+++ 大二專題/Lab/Homework 2-2/Top.v
@@ -1,22 +1,31 @@
 module Top (
+    input clk,
+	 input reset,
     input  wire [2:0] a,
     input  wire [2:0] b,
-    output wire dp,       // 控制小數點
-    output wire [6:0] seg_out,
-    output wire [3:0] an, // 控制第幾個七段顯示器
-    output wire [2:0] number_of_zero // 0 的數量用 LED 燈表示
+    output wire [6:0] sseg_out,
+    output wire [3:0] an
 );
     wire [2:0] sum;
     wire cout;
-    wire [2:0] number_of_one;
-    
+    wire [3:0] number_of_one_inter, number_of_zero_inter;
+
     RCA3 u1 (.a(a), .b(b), .sum(sum), .cout(cout));
 
-    assign number_of_one = sum[0] + sum[1] + sum[2] + cout;       // 計算 1 的數量
-    assign number_of_zero = 4 - number_of_one;  // 計算 0 的數量
+    assign number_of_one_inter = sum[0] + sum[1] + sum[2] + cout;       // 計算 1 的數量
+    assign number_of_zero_inter = 4 - number_of_one_inter;  // 計算 0 的數量
 
-    seven_segment_display u2 (.data_in(number_of_one), .seg_out(seg_out));
-    assign an = 4'b0111; // 左邊數來第二個七段顯示器負責顯示 1 的數量
-    assign dp = 1;
+    scan_led_hex_disp u2 (.clk(clk), 
+                        .reset(0), 
+                        .hex0(number_of_one_inter), 
+                        .hex1(number_of_zero_inter),
+                        .hex2(0),
+                        .hex3(0),
+                        .dp_in(0),
+                        .an(an),
+                        .sseg(sseg_out)
+                        );
+
+    
 
 endmodule //Top
---
+++ 大二專題/Lab/Homework 2-2/scan_led_hex_disp.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+module scan_led_hex_disp(
+    input clk,
+    input reset,
+    input [3:0] hex0, //第一个数码管显示的数字
+    input [3:0] hex1,
+    input [3:0] hex2,
+    input [3:0] hex3,
+    input [3:0] dp_in, //小数点控制
+    output reg [3:0] an,   //片选
+    output reg [6:0] sseg  //段选
+    );
+	
+	localparam N = 18; //使用低16位对50Mhz的时钟进行分频(50MHZ/2^16)
+	reg [N-1:0] regN; //高两位作为控制信号，低16位为计数器，对时钟进行分频
+	reg [3:0] hex_in; //段选控制信号
+	reg dp; 
+	
+	always@(posedge clk, posedge reset)
+	begin
+		if(reset)
+			regN <= 0;
+		else
+			regN <= regN + 1;
+	end
+	
+	always@ (*)
+	begin
+		case(regN[N-1:N-2])
+		2'b00:begin
+			an = 4'b1110; //选中第1个数码管
+			hex_in = hex0; //数码管显示的数字由hex_in控制，显示hex0输入的数字；
+			dp = dp_in[0]; //控制该数码管的小数点的亮灭
+		end
+		2'b01:begin
+			an = 4'b1101; //选中第二个数码管
+			hex_in = hex1;
+			dp = dp_in[1];
+		end
+		2'b10:begin
+			an = 4'b1011;
+			hex_in = hex2;
+			dp = dp_in[2];
+		end
+		default:begin
+			an = 4'b0111;
+			hex_in = hex3;
+			dp = dp_in[3];
+		end
+		
+		endcase
+	
+	end
+	always@ (*)
+	begin
+		case(hex_in)
+			4'h0: sseg[6:0] = 7'b011_1111; //共陰極
+			4'h1: sseg[6:0] = 7'b111_1001;
+			4'h2: sseg[6:0] = 7'b010_0100;
+			4'h3: sseg[6:0] = 7'b011_0000;
+			4'h4: sseg[6:0] = 7'b001_1001;
+			4'h5: sseg[6:0] = 7'b001_0010;
+			4'h6: sseg[6:0] = 7'b000_0010;
+			4'h7: sseg[6:0] = 7'b111_1000;
+			4'h8: sseg[6:0] = 7'b000_0000;
+			4'h9: sseg[6:0] = 7'b001_0000;
+			4'ha: sseg[6:0] = 7'b000_1000;
+			4'hb: sseg[6:0] = 7'b000_0011;
+			4'hc: sseg[6:0] = 7'b100_0110;	
+			4'hd: sseg[6:0] = 7'b010_0001;
+			4'he: sseg[6:0] = 7'b000_0110;
+			4'hf: sseg[6:0] = 7'b000_1110;
+			default: sseg[6:0] = 7'b100_0111;
+		endcase
+	end
+endmodule
