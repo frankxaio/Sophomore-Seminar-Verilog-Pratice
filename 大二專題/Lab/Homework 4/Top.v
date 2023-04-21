8e080e4a71656c4330081bcf607c4462a635b225
Xiao <frankxaio@gmail.com>
2023-04-21 13:16:47 +0800

    hw4

---
+++ 大二專題/Lab/Homework 4/Top.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+module Top (
+    input clk,
+    input reset,
+    output [6:0] seg_out,
+    output [3:0] an
+);
+
+    // 做累加需要用 DFF 
+    // 因為把 flip-flop 拿掉會造成combinational loop
+    // 造成電路會沒有穩態，變得無法控制，輸出都會變成unknown
+
+    reg [26:0] time_cnt;
+    reg [26:0] time_cnt_n;
+    reg [3:0] acc_reg;
+    reg [3:0] acc_reg_n;
+
+
+    parameter SET_TIME_1S = 27'd50_000_000;
+
+    
+    // 計數器
+    always @(posedge clk or negedge reset) begin
+        if (!reset) 
+            time_cnt <= 27'h0;
+        else 
+            time_cnt <= time_cnt_n;
+    end
+
+    // 
+    always @( *) begin
+        if (time_cnt == SET_TIME_1S)
+            time_cnt_n = 27'h0;
+        else 
+            time_cnt_n = time_cnt + 27'h1;
+    end
+
+
+
+    always @(posedge clk or negedge reset) begin
+        if (!reset)
+            acc_reg <= 4'b0000;
+        else
+            acc_reg <= acc_reg_n;
+    end
+
+    always @( *) begin
+        if (time_cnt == SET_TIME_1S) begin
+            if (acc_reg == 4'h9)
+                acc_reg_n = 4'h0;
+            else
+                acc_reg_n = acc_reg + 1'h1; 
+        end
+        else 
+            acc_reg_n = acc_reg;
+    end
+    
+    // 7-segment display
+    seven_segment_display u1 (.data_in(acc_reg), .seg_out(seg_out));
+    assign an = 4'b1011;
+
+
+endmodule //Top
---
+++ 大二專題/Lab/Homework 4/seven_segment_display.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+module seven_segment_display( data_in, seg_out );
+ 
+	input [3:0] data_in;
+	output reg[6:0] seg_out;
+ 
+	always@( * )
+		case( data_in )
+			4'h0 : seg_out = 7'b011_1111; 
+			4'h1 : seg_out = 7'b111_1001;
+			4'h2 : seg_out = 7'b010_0100;
+			4'h3 : seg_out = 7'b011_0000;
+			4'h4 : seg_out = 7'b001_1001;
+			4'h5 : seg_out = 7'b001_0010;
+			4'h6 : seg_out = 7'b000_0010;
+			4'h7 : seg_out = 7'b111_1000;
+			4'h8 : seg_out = 7'b000_0000;
+			4'h9 : seg_out = 7'b001_0000;
+			4'ha : seg_out = 7'b000_1000;
+			4'hb : seg_out = 7'b000_0011;
+			4'hc : seg_out = 7'b100_0110;
+			4'hd : seg_out = 7'b010_0001;
+			4'he : seg_out = 7'b000_0110;
+			4'hf : seg_out = 7'b000_1110;
+			default : seg_out =7'b100_0111;
+	endcase
+	
+endmodule
