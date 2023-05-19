2bd59d9721ed51571c7e2b1b3d61811bba4ba8e0
Xiao <frankxaio@gmail.com>
2023-05-19 15:53:52 +0800

    homework 9 1/4

--- Sophomore Seminar/Session 1/Homework 9/Homework9.m
+++ Sophomore Seminar/Session 1/Homework 9/Homework9.m
@@ -6,39 +6,12 @@ load weight;
 load input;
 
 for i  = 1:10
-    x = input(i,:) * weight + bias;
-    x(x<0) = 0;
-    y = x * weight2 + bias2;
-    [a,b] = max(y);
-    ans(i) = b; 
+    x = input(i,:) * weight   % + bias;
+    %x(x<0) = 0;
+    % y = x * weight2 + bias2;
+    %[a,b] = max(y);
+    %arr(i) = b; 
 end 
 
-fid = fopen('weight.txt','wt');   % data.txt為寫入檔名
-matrix = weight;                     % M為要儲存的矩陣
-[m,n]=size(matrix);                      
- for i=1:1:m
-   for j=1:1:n
-      if j==n
-        fprintf(fid,',%d\n',matrix(i,j));
-     else
-       fprintf(fid,',%d\t',matrix(i,j));
-      end
-   end
-end
-fclose(fid);
 
-
-fid = fopen('input.txt','wt');   % data.txt為寫入檔名
-matrix = input;                     % M為要儲存的矩陣
-[m,n]=size(matrix);                      
- for i=1:1:m
-   for j=1:1:n
-      if j==n
-        fprintf(fid,'%x\n',matrix(i,j));
-     else
-       fprintf(fid,'%x\t',matrix(i,j));
-      end
-   end
-end
-fclose(fid);
 
---
+++ Sophomore Seminar/Session 1/Homework 9/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for i in range(10):
+#     for j in range(20):
+#         print("Res1[%d][%d]," %(i,j), end=" ")
