f3b85cc1f569088bc5fb05bb7fda95944a732127
Xiao <frankxaio@gmail.com>
2023-05-19 22:44:56 +0800

    Update Homework9.m

--- Sophomore Seminar/Session 1/Homework 9/Matlab Code/Homework9.m
+++ Sophomore Seminar/Session 1/Homework 9/Matlab Code/Homework9.m
@@ -5,19 +5,10 @@ close all;
 load weight;
 load input;
 
-%for i  = 1:10
-    %x = input(i,:) * weight  + bias;
-    %x(x<0) = 0;
-    %y = x * weight2 + bias2;
-    %[a,b] = max(y);
-    %arr_max_index(i) = b; 
-    %arr_max(i) = a;
-%end 
-
-x = input(1,:) * weight  + bias;
-x(x<0) = 0;
-y = x * weight2 + bias2;
-
-
-
-
+for i  = 1:10
+    x = input(i,:) * weight + bias;
+    x(x<0) = 0;
+    y = x * weight2 + bias2;
+    [a,b] = max(y);
+    ans(i) = b; 
+end
